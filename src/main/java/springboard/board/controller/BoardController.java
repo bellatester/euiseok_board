679e8fed8f10c4fad3f4131b7dc9f2d5d6263372
심의석 <simsuli9@naver.com>
2021-01-22 15:23:40 +0900

    board post update using put mapping

--- src/main/java/springboard/board/controller/BoardController.java
+++ src/main/java/springboard/board/controller/BoardController.java
@@ -5,6 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import springboard.board.dto.BoardDto;
 import springboard.board.service.BoardService;
 
@@ -43,6 +44,19 @@ public class BoardController {
         BoardDto boardDto = boardService.getPost(id);
         model.addAttribute("post", boardDto);
         return "board/detail.html";
+    }
+
+    @GetMapping("/post/edit/{id}")
+    public String edit(@PathVariable("id") Long id, Model model){
+        BoardDto boardDto = boardService.getPost(id);
+        model.addAttribute("post", boardDto);
+        return "board/edit.html";
+    }
+
+    @PutMapping("/post/edit/{id}")
+    public String update(BoardDto boardDto){
+        boardService.savePost(boardDto);
+        return "redirect:/";
     }
 
 }
